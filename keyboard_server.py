3f2d5eb4bdc4153c1256c810dab811a417152a03
rexnfx <rexnfx611@gmail.com>
2026-02-03 19:29:43 -0600

    Update keyboard_server.py

--- keyboard_server.py
+++ keyboard_server.py
@@ -1,15 +1,9 @@
 
-import serial
 import keyboard
-import time
-from datetime import datetime, timedelta
-
-new_key = False
-next_send = datetime.now()
-input_msg = ''
-
 import serial
 import time
+
+input_msg = ''
 
 with serial.Serial('COM3', 115200, timeout=0.1) as ser:
     while True:
@@ -26,7 +20,7 @@ with serial.Serial('COM3', 115200, timeout=0.1) as ser:
             input_msg = b"N\n"
 
         # Write only when you actually have something to send
-        ser.write(b"Hello\n")
+        ser.write(input_msg)
         
         # Micro-polling loop for receiving
         # This keeps the script responsive without high CPU usage
@@ -38,32 +32,3 @@ with serial.Serial('COM3', 115200, timeout=0.1) as ser:
                 break # Exit micro-loop once data is found
             time.sleep(0.01) # Tiny sleep to free CPU
 
-while True:
-    if keyboard.is_pressed('left'):
-        if is_left:
-            next_send = datetime.now() + timedelta(milliseconds=200)
-            is_right = False
-        else:
-            new_key = False
-        print('L')
-        input_msg = b"R\n"
-        is_left = True
-        is_right = False
-        key_pressed = True
-
-    if keyboard.is_pressed('right'):
-        if is_right:
-            next_send = datetime.now() + timedelta(milliseconds=200)
-            is_left = False
-        else:
-            new_key = True
-        print('R')
-        input_msg = b"R\n"
-        is_left = False
-        is_right = True
-        key_pressed = True
-
-    if datetime.now() <= next_send and len(input_msg) > 0 and key_pressed:
-        ser.write(input_msg)
-
-    key_pressed = False
